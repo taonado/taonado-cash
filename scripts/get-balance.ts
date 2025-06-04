ed7cf19ec8f0e46ea1a885f3f9bec9e836fc6404
quintessenial <taonado@proton.me>
2025-06-04 15:53:09 -0400

    updated balance to be more useful

--- scripts/get-balance.ts
+++ scripts/get-balance.ts
@@ -1,11 +1,15 @@
 import { ethers } from "hardhat";
 import { config } from "../config";
 import { getTAOBalance, getWTAOBalance } from "./balance";
+import { convertH160ToSS58 } from "./address-utils";
 
 async function main() {
   // Get the wallet with provider
   const wallet = new ethers.Wallet(config.ethPrivateKey, ethers.provider);
   console.log("EVM Wallet:", wallet.address);
+
+  const ss58_address = convertH160ToSS58(wallet.address);
+  console.log("ss58 address:", ss58_address);
 
   const tao_balance = await getTAOBalance(wallet.address);
   console.log("TAO Balance:", tao_balance);
