33e70f622ac171497a11f0ab24186ec0833c6c07
quintessenial <taonado@proton.me>
2025-07-21 20:31:18 -0400

    base withdrawal tests

--- test/taonado.test.ts
+++ test/taonado.test.ts
@@ -1,8 +1,6 @@
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import hardhat from "hardhat";
-import { ethers as eth } from "ethers";
 import { compileHasher } from "../tools/hasher";
 import {
   createDeposit,
@@ -11,10 +9,6 @@ import {
   toHex,
   generateSnarkProof,
 } from "../core/cli";
-import {
-  DepositEvent,
-  ERC20Taonado,
-} from "../typechain-types/core/ERC20Taonado";
 
 const pool_token_amount = ethers.parseEther("1");
 const MERKLE_TREE_HEIGHT = 20;
@@ -133,6 +127,67 @@ describe("ERC20Taonado", function () {
       await expect(
         taonado_erc20.deposit(toHex(deposit.commitment))
       ).to.be.rejectedWith("FailedInnerCall()");
+    });
+  });
+
+  describe("Withdrawals", function () {
+    async function setupAndMakeDeposit() {
+      const { wtao, taonado_erc20 } = await loadFixture(deployERC20Taonado);
+      const [wallet] = await ethers.getSigners();
+
+      await wtao.deposit({ value: pool_token_amount });
+      await wtao.approve(taonado_erc20.getAddress(), pool_token_amount);
+
+      const { deposit, note } = await createDeposit();
+      await taonado_erc20.deposit(toHex(deposit.commitment));
+
+      // generate a proof and use it across multiple tests, speeding up the tests
+      const { proof, args } = await generateSnarkProof(
+        deposit,
+        wallet.address,
+        taonado_erc20
+      );
+
+      return { wtao, taonado_erc20, wallet, deposit, note, proof, args };
+    }
+    it("should allow withdrawals", async function () {
+      const { wtao, taonado_erc20, wallet, deposit, note, proof, args } =
+        await loadFixture(setupAndMakeDeposit);
+
+      const beforeBalance = await wtao.balanceOf(wallet.address);
+      // @ts-ignore
+      await taonado_erc20.withdraw(proof, ...args);
+      const afterBalance = await wtao.balanceOf(wallet.address);
+
+      expect(afterBalance).to.equal(beforeBalance + pool_token_amount);
+    });
+
+    it("should prevent double withdrawals", async function () {
+      const { wtao, taonado_erc20, wallet, deposit, note, proof, args } =
+        await loadFixture(setupAndMakeDeposit);
+
+      // @ts-ignore
+      await taonado_erc20.withdraw(proof, ...args);
+
+      await expect(
+        // @ts-ignore
+        taonado_erc20.withdraw(proof, ...args)
+      ).to.be.revertedWith("The note has been already spent");
+    });
+
+    it("should not allow recipient change", async function () {
+      const { wtao, taonado_erc20, wallet, deposit, note, proof, args } =
+        await loadFixture(setupAndMakeDeposit);
+
+      const [, malicious] = await ethers.getSigners();
+
+      const badArgs = [proof, ...args];
+      badArgs[3] = malicious.address;
+
+      await expect(
+        // @ts-ignore
+        taonado_erc20.withdraw(...badArgs)
+      ).to.be.revertedWith("Invalid withdraw proof");
     });
   });
 });
