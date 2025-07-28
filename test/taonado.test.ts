85c155ddc1fc091904b94f1006c9c3618317c628
quintessenial <taonado@proton.me>
2025-07-28 12:54:17 -0400

    consistency update

--- test/taonado.test.ts
+++ test/taonado.test.ts
@@ -10,6 +10,7 @@ import {
   toHex,
   generateSnarkProof,
 } from "../core/taonado";
+import { Contracts } from "../scripts/contracts";
 
 const pool_token_amount = ethers.parseEther("1");
 const { MERKLE_TREE_HEIGHT } = config;
@@ -19,13 +20,17 @@ describe("ERC20Taonado", function () {
     const [deployer] = await ethers.getSigners();
     const hasher = await deployHasher(deployer);
 
-    const verifier_factory = await ethers.getContractFactory("Verifier");
+    const verifier_factory = await ethers.getContractFactory(
+      Contracts.VERIFIER
+    );
     const verifier = await verifier_factory.deploy();
 
-    const wtao_factory = await ethers.getContractFactory("WTAO");
+    const wtao_factory = await ethers.getContractFactory(Contracts.WTAO);
     const wtao = await wtao_factory.deploy();
 
-    const taonado_factory = await ethers.getContractFactory("ERC20Taonado");
+    const taonado_factory = await ethers.getContractFactory(
+      Contracts.ERC20TAONADO
+    );
     const taonado_erc20 = await taonado_factory.deploy(
       verifier.getAddress(),
       hasher.getAddress(),
